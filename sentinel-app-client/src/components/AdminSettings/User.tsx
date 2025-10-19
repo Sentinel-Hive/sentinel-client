ae4b360355e9756a5d9f9e474463fd2623cc47ff
Austin Adams <asadams110102@gmail.com>
2025-10-19 15:42:08 -0500

    current work/design

--- sentinel-app-client/src/components/AdminSettings/User.tsx
+++ sentinel-app-client/src/components/AdminSettings/User.tsx
@@ -1,14 +1,28 @@
+import { useState } from "react";
+
+type User = {
+    id: number;
+    user_id: string;
+    is_admin: boolean;
+    last_login?: Date;
+};
+
 export default function User() {
-    const fakeUsers = [
+    const fakeUsers: User[] = [
         { id: 0, user_id: "user0", is_admin: true },
         { id: 1, user_id: "user1", is_admin: false },
         { id: 2, user_id: "user2", is_admin: false },
         { id: 3, user_id: "user3", is_admin: false },
     ];
+    const [userList, setUserList] = useState<User[]>(fakeUsers);
 
     return (
         <div className="flex w-full h-full border-yellow-600 border rounded-md p-5">
-            <p>hello users</p>
+            {userList.map((u, i) => (
+                <p key={i}>
+                    {u.id},{u.user_id}
+                </p>
+            ))}
         </div>
     );
 }
